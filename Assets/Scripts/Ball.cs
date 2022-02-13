f1d0a85fd9f6d207c75f7ac5b3668d5ff6ebc0d5
Fzhut0 <80861162+Fzhut0@users.noreply.github.com>
2022-02-13 17:57:07 +0100

    Added checking matches, destroying matches, moving ball positions

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,8 +6,12 @@ public class Ball : MonoBehaviour
 {
     BallData data;
 
+
     public int column;
     public int row;
+
+    public int previousColumn;
+    public int previousRow;
 
     public int targetX;
     public int targetY;
@@ -21,6 +25,7 @@ public class Ball : MonoBehaviour
 
     public bool isMatched = false;
     public float swipeAngle = 0;
+    public float swipeResist = 1f;
 
     private void Awake()
     {
@@ -30,12 +35,15 @@ public class Ball : MonoBehaviour
     private void Start()
     {
         board = FindObjectOfType<BoardManager>();
+
         targetX = (int)transform.position.x;
         targetY = (int)transform.position.y;
 
-
         row = targetY;
         column = targetX;
+
+        previousColumn = column;
+        previousRow = row;
     }
 
     private void Update()
@@ -51,24 +59,30 @@ public class Ball : MonoBehaviour
         if (Mathf.Abs(targetX - transform.position.x) > .1)
         {
             tempPos = new Vector2(targetX, transform.position.y);
-            transform.position = Vector2.Lerp(transform.position, tempPos, .4f);
+            transform.position = Vector2.Lerp(transform.position, tempPos, .6f);
+            if (board.allBalls[column, row] != gameObject)
+            {
+                board.allBalls[column, row] = gameObject;
+            }
         }
         else
         {
             tempPos = new Vector2(targetX, transform.position.y);
             transform.position = tempPos;
-            board.allBalls[column, row] = gameObject.gameObject;
         }
-        if (Mathf.Abs(targetX - transform.position.x) > .1)
+        if (Mathf.Abs(targetY - transform.position.y) > .1)
         {
             tempPos = new Vector2(transform.position.x, targetY);
-            transform.position = Vector2.Lerp(transform.position, tempPos, .4f);
+            transform.position = Vector2.Lerp(transform.position, tempPos, .6f);
+            if (board.allBalls[column, row] != gameObject)
+            {
+                board.allBalls[column, row] = gameObject;
+            }
         }
         else
         {
             tempPos = new Vector2(transform.position.x, targetY);
             transform.position = tempPos;
-            board.allBalls[column, row] = gameObject.gameObject;
         }
 
 
@@ -87,20 +101,23 @@ public class Ball : MonoBehaviour
 
     void CalculateAngle()
     {
-        swipeAngle = Mathf.Atan2(finalTouchPos.y - firstTouchPos.x, finalTouchPos.x - firstTouchPos.y) * 180 / Mathf.PI;
-        MovePieces();
+        if (Mathf.Abs(finalTouchPos.y - firstTouchPos.y) > swipeResist || Mathf.Abs(finalTouchPos.x - firstTouchPos.x) > swipeResist)
+        {
+            swipeAngle = Mathf.Atan2(finalTouchPos.y - firstTouchPos.y, finalTouchPos.x - firstTouchPos.x) * 180 / Mathf.PI;
+            MovePieces();
+        }
     }
 
     void MovePieces()
     {
-        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width)
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width - 1)
         {
             // right
             otherBall = board.allBalls[column + 1, row];
             otherBall.GetComponent<Ball>().column -= 1;
             column += 1;
         }
-        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height - 1)
         {
             // up
             otherBall = board.allBalls[column, row + 1];
@@ -121,6 +138,7 @@ public class Ball : MonoBehaviour
             otherBall.GetComponent<Ball>().row += 1;
             row -= 1;
         }
+        StartCoroutine(CheckMove());
     }
 
     void FindMatches()
@@ -129,26 +147,49 @@ public class Ball : MonoBehaviour
         {
             GameObject leftDot1 = board.allBalls[column - 1, row];
             GameObject rightDot1 = board.allBalls[column + 1, row];
-            if (leftDot1.tag == gameObject.tag && rightDot1.tag == gameObject.tag)
+            if (leftDot1 != null && rightDot1 != null)
             {
-                leftDot1.GetComponent<Ball>().isMatched = true;
-                rightDot1.GetComponent<Ball>().isMatched = true;
-                isMatched = true;
+                if (leftDot1.tag == gameObject.tag && rightDot1.tag == gameObject.tag)
+                {
+                    leftDot1.GetComponent<Ball>().isMatched = true;
+                    rightDot1.GetComponent<Ball>().isMatched = true;
+                    isMatched = true;
+                }
             }
         }
         if (row > 0 && row < board.height - 1)
         {
             GameObject upDot1 = board.allBalls[column, row + 1];
             GameObject downDot1 = board.allBalls[column, row - 1];
-            if (upDot1.tag == gameObject.tag && downDot1.tag == gameObject.tag)
+            if (upDot1 != null && downDot1 != null)
             {
-                upDot1.GetComponent<Ball>().isMatched = true;
-                downDot1.GetComponent<Ball>().isMatched = true;
-                isMatched = true;
+                if (upDot1.tag == gameObject.tag && downDot1.tag == gameObject.tag)
+                {
+                    upDot1.GetComponent<Ball>().isMatched = true;
+                    downDot1.GetComponent<Ball>().isMatched = true;
+                    isMatched = true;
+                }
             }
-
         }
-
-
     }
+    IEnumerator CheckMove()
+    {
+        yield return new WaitForSeconds(.5f);
+        if (otherBall != null)
+        {
+            if (!isMatched && !otherBall.GetComponent<Ball>().isMatched)
+            {
+                otherBall.GetComponent<Ball>().column = column;
+                otherBall.GetComponent<Ball>().row = row;
+                row = previousRow;
+                column = previousColumn;
+            }
+            else
+            {
+                board.CheckDestroyMatches();
+            }
+        }
+        otherBall = null;
+    }
+
 }
--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -36,16 +36,97 @@ public class BoardManager : MonoBehaviour
                 backgroundTile.transform.parent = gameObject.transform;
                 backgroundTile.name = "(" + x + "," + y + ")";
                 int ballToUse = Random.Range(0, balls.Length);
+                int maxIterations = 0;
+                while (SpawnMatchCheck(x, y, balls[ballToUse]) && maxIterations < 100)
+                {
+                    ballToUse = Random.Range(0, balls.Length);
+                    maxIterations++;
 
+                }
                 GameObject ball = Instantiate(balls[ballToUse], spawnPos, Quaternion.identity);
                 ball.transform.parent = backgroundTile.transform;
                 ball.name = gameObject.name;
                 allBalls[x, y] = ball;
             }
         }
-
-
     }
 
+    private bool SpawnMatchCheck(int column, int row, GameObject tile)
+    {
+        if (column > 1 && row > 1)
+        {
+            if (allBalls[column - 1, row].tag == tile.tag && allBalls[column - 2, row])
+            {
+                return true;
+            }
+            if (allBalls[column, row - 1].tag == tile.tag && allBalls[column, row - 2])
+            {
+                return true;
+            }
+            else if (column <= 1 || row <= 1)
+            {
+                if (row > 1)
+                {
+                    if (allBalls[column, row - 1].tag == tile.tag && allBalls[column, row - 2].tag == tile.tag)
+                    {
+                        return true;
+                    }
+                }
+                if (column > 1)
+                {
+                    if (allBalls[column - 1, row].tag == tile.tag && allBalls[column - 2, row].tag == tile.tag)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
+    void DestroyMatches(int column, int row)
+    {
+        if (allBalls[column, row].GetComponent<Ball>().isMatched)
+        {
+            Destroy(allBalls[column, row]);
+            allBalls[column, row] = null;
+        }
+    }
+
+    public void CheckDestroyMatches()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (allBalls[x, y] != null)
+                {
+                    DestroyMatches(x, y);
+                }
+            }
+        }
+        StartCoroutine(DecreaseRow());
+    }
+
+    IEnumerator DecreaseRow()
+    {
+        int nullCount = 0;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (allBalls[x, y] == null)
+                {
+                    nullCount++;
+                }
+                else if (nullCount > 0)
+                {
+                    allBalls[x, y].GetComponent<Ball>().row -= nullCount;
+                    allBalls[x, y] = null;
+                }
+            }
+            nullCount = 0;
+        }
+        yield return new WaitForSeconds(.4f);
+    }
 }
